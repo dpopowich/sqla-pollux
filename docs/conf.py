08e6fd8b500b4ac5f73eb1e2cd313fd8d2ada8af
dpopowich <danielpopowich@gmail.com>
2026-04-10 16:53:06 -0400

    Add conf.py file with project configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,30 +1,10 @@
-# Configuration file for the Sphinx documentation builder
-
-# -- General configuration ------------------------------------------------
-# If extensions (or modules to document with autodoc) are in another directory,
-you can add these directories to sys.path here.  If the directory is relative to
-the documentation root, use os.path.abspath to make it absolute, like shown here.
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
-
-# -- Project information -----------------------------------------------------
-project = 'sqla-pollux'
-author = 'Your Name'
-copyright = '2023, Your Name'
-
-# -- General configuration ---------------------------------------------------
-extensions = []
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# The suffix of source filenames.
-suffix = '.rst'
-
-# The master toctree document.
-master_doc = 'index'
-
-# -- Options for HTML output -------------------------------------------------
-html_theme = 'alabaster'
+project = "sqla-pollux"
+copyright = "2026, Daniel Popowich"
+author = "Daniel Popowich"
+extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+myst_enable_extensions = ["colon_fence", "deflist"]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {"logo_only": False, "display_version": True}
+myst_heading_anchors = 3
